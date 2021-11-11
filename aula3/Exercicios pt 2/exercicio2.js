15c0a331085b94a84c6e3b1bff9913fc66d3ce50
Thais Barbosa <thaismsbrasil@hotmail.com>
2021-11-10 22:56:23 -0300

    atividades aula 3

--- aula3/Exercicios pt 2/exercicio2.js
+++ aula3/Exercicios pt 2/exercicio2.js
@@ -3,7 +3,7 @@ var saldoComBonus = saldoFinal + 20;
 var saldoComDesconto = saldoBonus / 0.5;
 var saldoFinal = saldoComDesconto;
 
-//Acredito que ouve erro de digitação na questão, daria certo da forma abaixo:
+//Acredito que houve erro de digitação na questão, daria certo da forma abaixo:
 // var saldo = 50;
 // var saldoComBonus = saldo + 20;
 // var saldoComDesconto = saldoComBonus / 0.5;
